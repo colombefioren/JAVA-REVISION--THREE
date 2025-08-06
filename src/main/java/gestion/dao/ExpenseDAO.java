4ec2d13c2f8a161747c9aa0893077a79354aaddc
colombefioren <colomberakotonjanahary@gmail.com>
2025-08-06 17:08:46 +0300

    feat: ExpenseDAO class inside a newly created dao package

---
+++ src/main/java/gestion/dao/ExpenseDAO.java
@@ -0,0 +1,49 @@
+package gestion.dao;
+
+import gestion.entity.Expense;
+import gestion.entity.RecurringExpense;
+import gestion.entity.RefundableExpense;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ExpenseDAO {
+  private final List<Expense> expenses;
+
+  public ExpenseDAO(List<Expense> expenses) {
+    this.expenses = expenses;
+  }
+
+  public List<Expense> getExpenses() {
+        return expenses;
+  }
+
+  public List<Expense> getPendingRefunds(){
+      return expenses.stream().filter(e -> !((RefundableExpense) e).isRefunded()).toList();
+  }
+
+  public double getTotalRecurringAmount(){
+      return expenses.stream().filter(e -> e instanceof RecurringExpense).mapToDouble(Expense::getAmount).sum();
+  }
+
+  public List<String> getHighLabelExpenses(){
+      return expenses.stream().filter(Expense::isLargeExpense).map(Expense::getName).toList();
+  }
+
+  public double getTotalExpensesAmount(){
+      return expenses.stream().mapToDouble(Expense::getAmount).sum();
+  }
+
+  public Map<String,Double> groupTotalByMonth(){
+      Map<String,Double> result = new HashMap<>();
+      expenses.forEach(e -> {
+          String key = e.getYearMonth();
+          if(result.containsKey(key)){
+              result.put(key, result.get(key) + e.getAmount());
+          }else{
+              result.put(key, e.getAmount());
+          }
+      });
+      return result;
+  }
+}
